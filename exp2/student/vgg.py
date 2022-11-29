d9bd819b546f7133a8d2965dc8202979e441f5de
carajzp <jsnjpengpeng@126.com>
2022-11-29 17:20:24 +0800

    exp2

---
+++ exp2/student/vgg.py
@@ -0,0 +1,62 @@
+from layer import ConvolutionLayer, ReluLayer, MaxPoolLayer, FullyConnectLayer, FlattenLayer
+
+
+class VGG16(object):
+    def __init__(self, num_classes=4):
+        # TODO 根据网络图搭建VGG16模型,命名需按照self.graph_layers下命名规范。
+        self.layer1_conv1 = ConvolutionLayer(in_channels=3, out_channels=64, kernel_size=3, padding=1)
+        self.layer1_relu1 = ReluLayer()
+        self.layer1_conv2 = ConvolutionLayer(in_channels=64, out_channels=64, kernel_size=3, padding=1)
+        self.layer1_relu2 = ReluLayer()
+        self.layer1_maxpool = MaxPoolLayer(kernel_size=2, stride=2)
+
+        self.graph_layers = None
+        self.create_graph()
+
+    def create_graph(self):
+        self.graph_layers = {
+            'layer1_conv1': self.layer1_conv1, 'layer1_relu1': self.layer1_relu1,
+            'layer1_conv2': self.layer1_conv2, 'layer1_relu2': self.layer1_relu2,
+            'layer1_maxpool': self.layer1_maxpool,
+
+            'layer2_conv1': self.layer2_conv1, 'layer2_relu1': self.layer2_relu1,
+            'layer2_conv2': self.layer2_conv2, 'layer2_relu2': self.layer2_relu2,
+            'layer2_maxpool': self.layer2_maxpool,
+
+            'layer3_conv1': self.layer3_conv1, 'layer3_relu1': self.layer3_relu1,
+            'layer3_conv2': self.layer3_conv2, 'layer3_relu2': self.layer3_relu2,
+            'layer3_conv3': self.layer3_conv3, 'layer3_relu3': self.layer3_relu3,
+            'layer3_maxpool': self.layer3_maxpool,
+
+            'layer4_conv1': self.layer4_conv1, 'layer4_relu1': self.layer4_relu1,
+            'layer4_conv2': self.layer4_conv2, 'layer4_relu2': self.layer4_relu2,
+            'layer4_conv3': self.layer4_conv3, 'layer4_relu3': self.layer4_relu3,
+            'layer4_maxpool': self.layer4_maxpool,
+
+            'layer5_conv1': self.layer5_conv1, 'layer5_relu1': self.layer5_relu1,
+            'layer5_conv2': self.layer5_conv2, 'layer5_relu2': self.layer5_relu2,
+            'layer5_conv3': self.layer5_conv3, 'layer5_relu3': self.layer5_relu3,
+            'layer5_maxpool': self.layer5_maxpool,
+
+            'flatten': self.flatten,
+            'fullyconnect1': self.fullyconnect1, 'relu1': self.relu_1,
+            'fullyconnect2': self.fullyconnect2, 'relu2': self.relu_2,
+            'fullyconnect3': self.fullyconnect3,
+        }
+
+    def forward(self, x):
+        for name in self.graph_layers.keys():
+            print(f'forward: {name}: {x.mean()} {x.sum()}')
+            x = self.graph_layers[name].forward(x)
+        return x
+
+    def backward(self, grad):
+        for name in reversed(list(self.graph_layers.keys())):
+            print(f'backward: {name}: {grad.mean()} {grad.sum()}')
+            grad = self.graph_layers[name].backward(grad)
+        return grad
+
+    def resume_weights(self, ckpt):
+        for name, params in ckpt.items():
+            self.graph_layers[name].load_params(params['weight'], params['bias'])
+        print('reloaded success')
---
+++ exp2/teacher/main.py
@@ -0,0 +1,78 @@
+import cv2
+import numpy as np
+from vgg import VGG16
+from layer import CrossEntropy
+
+
+def resize_image(image, target_size):
+    h, w = image.shape[:2]
+    th, tw = target_size
+
+    # 获取等比缩放后的尺寸
+    scale = min(th / h, tw / w)
+    oh, ow = round(h * scale), round(w * scale)
+
+    # 缩放图片，opencv缩放传入尺寸为（宽，高），这里采用线性差值算法
+    image = cv2.resize(image, (ow, oh), interpolation=cv2.INTER_LINEAR).astype(np.uint8)
+
+    # 将剩余部分进行填充
+    new_image = np.ones((th, tw, 3), dtype=np.uint8) * 114
+    new_image[:oh, :ow, :] = image
+    return new_image
+
+
+def process_image(img_path):
+    # 读取图片，opencv读图后格式是BGR格式，需要转为RGB格式
+    image = cv2.imread(img_path, cv2.IMREAD_COLOR)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    # 将图片等比resize至(224x224)
+    image = resize_image(image, (224, 224))
+    image = np.array(image, dtype=np.float32)
+
+    # 将图片标准化
+    image -= [125.307, 122.961, 113.8575]
+    image /= [51.5865, 50.847, 51.255]
+
+    # (h,w,c) -> (c,h,w) -> (1,c,h,w)
+    image = image.transpose((2, 0, 1))[None]
+    return image
+
+
+if __name__ == '__main__':
+    import time
+    # 分类类别
+    CLASSES = ('daisy', 'roses', 'sunflowers', 'tulips')
+
+    # 网络初始化、加载权重参数
+    model = VGG16(4)
+    ckpt = np.load('./file/vgg16_ckpt.npy', allow_pickle=True).item()
+    model.resume_weights(ckpt)
+
+    start_time = time.time()
+
+    # 输入图片预处理
+    image_path = './file/tulips_demo.jpg'
+    tensor = process_image(image_path)
+
+    # 模型正向传播
+    outputs = model.forward(tensor)
+    print(outputs)
+    pred = int(np.argmax(outputs))
+    print(CLASSES[pred])
+
+    # 计算loss
+    label = np.array([1, ])
+    loss_func = CrossEntropy()
+    loss = loss_func.forward(outputs, label)
+    print(loss)
+
+    # 反向传播
+    grad = loss_func.backward()
+    grad = model.backward(grad)
+    print(grad.shape)
+    print(grad.mean(), grad.sum())
+
+    end_time = time.time()
+    print(end_time - start_time)
+
